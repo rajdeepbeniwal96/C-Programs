7700114b3c2333b4daac89354c0ed612302cf850
Rajdeep Beniwal <rajdeepbeniwal96@gmail.com>
2026-09-14 21:26:04 +0530

    Right aligned ascending number triangle pattern

---
+++ Pattern Printing/Right aligned ascending number triangle.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int row,col;
+    int n;
+    cout<<"Enter a number: ";
+    cin>>n;
+
+    for(row=1;row<=n;row++){
+        for(col=1;col<=n-row;col++){
+            cout<<"  ";
+        }
+        for(col=1;col<=row;col++){
+            cout<<col<<" ";
+        }
+        cout<<endl;
+    }
+    return 0;
+}
