d4bdd9e497048c1adddc716d79c73f144b017f3e
Rajdeep Beniwal <rajdeepbeniwal96@gmail.com>
2026-09-14 20:06:13 +0530

    Repeated alphabet rows pattern

--- Pattern Printing/Repeated alphabet rows.cpp
+++ Pattern Printing/Repeated alphabet rows.cpp
@@ -2,24 +2,24 @@
 using namespace std;
 
 /*
-A B C . . n
-A B C . . n
-A B C . . n
+A A A . . n
+B B B . . n
+C C C . . n
 . . . . . n
 . . . . . n
 til n times
 */
 
 int main(){
-    int row;
+    char row;
     char col;
     int n;
     cout<<"Enter the alphabets till u want to print the pattern: ";
     cin>>n;
 
-    for(row=1;row<=n;row++){
-        for(col='A';col<=char(n+64);col++){
-            cout<<col<<" "; //Explicit type conversion.
+    for(row='A';row<=char(n+64);row++){
+        for(col=1;col<=n;col++){
+            cout<<row<<" "; //Explicit type conversion.
             //for small alphabets use char(col+96).
         }
         cout<<endl;
