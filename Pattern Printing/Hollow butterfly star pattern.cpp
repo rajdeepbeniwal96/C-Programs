b3e972ad4302631b9b9f7989131cb3cee8d676a4
Rajdeep Beniwal <rajdeepbeniwal96@gmail.com>
2026-09-15 12:51:30 +0530

    Add hollow butterfly star pattern

---
+++ Pattern Printing/Hollow butterfly star pattern.cpp
@@ -0,0 +1,41 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int row, col;
+    int n;
+    cout << "Enter a number: ";
+    cin >> n;
+
+    for (row = 1; row <= n - 1; row++) {
+        for (col = 1; col <= row; col++) {
+            cout << "* ";
+        }
+
+        for (col = 1; col <= (n - 1) * 2 - (2 * row); col++) {
+            cout << "  ";
+        }
+
+        for (col = 1; col <= row; col++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+
+    for (row = n - 2; row >= 1; row--) {
+        for (col = 1; col <= row; col++) {
+            cout << "* ";
+        }
+
+        for (col = 1; col <= (n - 1) * 2 - (2 * row); col++) {
+            cout << "  ";
+        }
+
+        for (col = 1; col <= row; col++) {
+            cout << "* ";
+        }
+        cout << endl;
+    }
+    return 0;
+}
